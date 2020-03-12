Archinect/Paradise

Фикс максимальной длины при переименовании
Бумажки и их стаки
Фотографии
Факсы
Турели

code/game/machinery/portable_turret.dm
@@ -903,7 +903,7 @@ var/list/turret_icons
 
 	if(istype(I, /obj/item/pen))	//you can rename turrets like bots!
 		var/t = input(user, "Enter new turret name", name, finish_name) as text
-		t = sanitize(copytext_char(t, 1, MAX_MESSAGE_LEN))
+		t = sanitize(copytext_char(t, 1, MAX_NAME_LEN))
 		if(!t)
 			return
 		if(!in_range(src, usr) && loc != usr)

code/modules/paperwork/faxmachine.dm
@@ -193,7 +193,7 @@ var/global/list/fax_blacklist = list()
 
 	if(href_list["rename"])
 		if(copyitem)
-			var/n_name = sanitize(copytext_char(input(usr, "What would you like to label the fax?", "Fax Labelling", copyitem.name)  as text, 1, MAX_MESSAGE_LEN))
+			var/n_name = sanitize(copytext_char(input(usr, "What would you like to label the fax?", "Fax Labelling", copyitem.name)  as text, 1, MAX_NAME_LEN))
 			if((copyitem && copyitem.loc == src && usr.stat == 0))
 				if(istype(copyitem, /obj/item/paper))
 					copyitem.name = "[(n_name ? text("[n_name]") : initial(copyitem.name))]"

code/modules/paperwork/paper.dm
@@ -100,7 +100,7 @@
 	if(!usr.is_literate())
 		to_chat(usr, "<span class='notice'>You don't know how to read.</span>")
 		return
-	var/n_name = sanitize(copytext_char(input(usr, "What would you like to label the paper?", "Paper Labelling", name) as text, 1, MAX_MESSAGE_LEN))
+	var/n_name = sanitize(copytext_char(input(usr, "What would you like to label the paper?", "Paper Labelling", name) as text, 1, MAX_NAME_LEN))
 	if((loc == usr && usr.stat == 0))
 		name = "[(n_name ? text("[n_name]") : initial(name))]"
 	if(name != "paper")

code/modules/paperwork/paper_bundle.dm
@@ -192,7 +192,7 @@
 	set category = "Object"
 	set src in usr
 
-	var/n_name = sanitize(copytext_char(input(usr, "What would you like to label the bundle?", "Bundle Labelling", name) as text, 1, MAX_MESSAGE_LEN))
+	var/n_name = sanitize(copytext_char(input(usr, "What would you like to label the bundle?", "Bundle Labelling", name) as text, 1, MAX_NAME_LEN))
 	if((loc == usr && usr.stat == 0))
 		name = "[(n_name ? text("[n_name]") : "paper bundle")]"
 	add_fingerprint(usr)

code/modules/paperwork/photography.dm
@@ -95,7 +95,7 @@
 	set category = "Object"
 	set src in usr
 
-	var/n_name = sanitize(copytext_char(input(usr, "What would you like to label the photo?", "Photo Labelling", name) as text, 1, MAX_MESSAGE_LEN))
+	var/n_name = sanitize(copytext_char(input(usr, "What would you like to label the photo?", "Photo Labelling", name) as text, 1, MAX_NAME_LEN))
 	//loc.loc check is for making possible renaming photos in clipboards
 	if(( (loc == usr || (loc.loc && loc.loc == usr)) && usr.stat == 0))
 		name = "[(n_name ? text("[n_name]") : "photo")]"

egordmh/Paradise-old

code/game/machinery/portable_turret.dm
@@ -903,7 +903,7 @@ var/list/turret_icons
 
 	if(istype(I, /obj/item/pen))	//you can rename turrets like bots!
 		var/t = input(user, "Enter new turret name", name, finish_name) as text
-		t = sanitize(copytext(t, 1, MAX_MESSAGE_LEN))
+		t = sanitize(copytext(t, 1, MAX_NAME_LEN))
 		if(!t)
 			return
 		if(!in_range(src, usr) && loc != usr)

code/modules/paperwork/faxmachine.dm
@@ -193,7 +193,7 @@ var/global/list/fax_blacklist = list()
 
 	if(href_list["rename"])
 		if(copyitem)
-			var/n_name = sanitize(copytext(input(usr, "What would you like to label the fax?", "Fax Labelling", copyitem.name)  as text, 1, MAX_MESSAGE_LEN))
+			var/n_name = sanitize(copytext(input(usr, "What would you like to label the fax?", "Fax Labelling", copyitem.name)  as text, 1, MAX_NAME_LEN))
 			if((copyitem && copyitem.loc == src && usr.stat == 0))
 				if(istype(copyitem, /obj/item/paper))
 					copyitem.name = "[(n_name ? text("[n_name]") : initial(copyitem.name))]"

code/modules/paperwork/paper.dm
@@ -100,7 +100,7 @@
 	if(!usr.is_literate())
 		to_chat(usr, "<span class='notice'>You don't know how to read.</span>")
 		return
-	var/n_name = sanitize(copytext(input(usr, "What would you like to label the paper?", "Paper Labelling", name) as text, 1, MAX_MESSAGE_LEN))
+	var/n_name = sanitize(copytext(input(usr, "What would you like to label the paper?", "Paper Labelling", name) as text, 1, MAX_NAME_LEN))
 	if((loc == usr && usr.stat == 0))
 		name = "[(n_name ? text("[n_name]") : initial(name))]"
 	if(name != "paper")

code/modules/paperwork/paper_bundle.dm
@@ -192,7 +192,7 @@
 	set category = "Object"
 	set src in usr
 
-	var/n_name = sanitize(copytext(input(usr, "What would you like to label the bundle?", "Bundle Labelling", name) as text, 1, MAX_MESSAGE_LEN))
+	var/n_name = sanitize(copytext(input(usr, "What would you like to label the bundle?", "Bundle Labelling", name) as text, 1, MAX_NAME_LEN))
 	if((loc == usr && usr.stat == 0))
 		name = "[(n_name ? text("[n_name]") : "paper bundle")]"
 	add_fingerprint(usr)

code/modules/paperwork/photography.dm
@@ -95,7 +95,7 @@
 	set category = "Object"
 	set src in usr
 
-	var/n_name = sanitize(copytext(input(usr, "What would you like to label the photo?", "Photo Labelling", name) as text, 1, MAX_MESSAGE_LEN))
+	var/n_name = sanitize(copytext(input(usr, "What would you like to label the photo?", "Photo Labelling", name) as text, 1, MAX_NAME_LEN))
 	//loc.loc check is for making possible renaming photos in clipboards
 	if(( (loc == usr || (loc.loc && loc.loc == usr)) && usr.stat == 0))
 		name = "[(n_name ? text("[n_name]") : "photo")]"