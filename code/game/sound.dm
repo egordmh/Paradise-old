RV666/Paradise

Звуковые эффекты для зон как на Бэе

code/game/sound.dm
@@ -1,4 +1,42 @@
-/proc/playsound(atom/source, soundin, vol as num, vary, extrarange as num, falloff, frequency = null, channel = 0, pressure_affected = TRUE, ignore_walls = TRUE)
+//Sound environment defines. Reverb preset for sounds played in an area, see sound datum reference for more.
+#define GENERIC 0
+#define PADDED_CELL 1
+#define ROOM 2
+#define BATHROOM 3
+#define LIVINGROOM 4
+#define STONEROOM 5
+#define AUDITORIUM 6
+#define CONCERT_HALL 7
+#define CAVE 8
+#define ARENA 9
+#define HANGAR 10
+#define CARPETED_HALLWAY 11
+#define HALLWAY 12
+#define STONE_CORRIDOR 13
+#define ALLEY 14
+#define FOREST 15
+#define CITY 16
+#define MOUNTAINS 17
+#define QUARRY 18
+#define PLAIN 19
+#define PARKING_LOT 20
+#define SEWER_PIPE 21
+#define UNDERWATER 22
+#define DRUGGED 23
+#define DIZZY 24
+#define PSYCHOTIC 25
+
+#define STANDARD_STATION STONEROOM
+#define LARGE_ENCLOSED HANGAR
+#define SMALL_ENCLOSED BATHROOM
+#define TUNNEL_ENCLOSED CAVE
+#define LARGE_SOFTFLOOR CARPETED_HALLWAY
+#define MEDIUM_SOFTFLOOR LIVINGROOM
+#define SMALL_SOFTFLOOR ROOM
+#define ASTEROID CAVE
+#define SPACE UNDERWATER
+
+/proc/playsound(atom/source, soundin, vol as num, vary, extrarange as num, falloff, frequency = null, channel = 0, pressure_affected = TRUE, ignore_walls = TRUE, var/is_global)
 	if(isarea(source))
 		error("[source] is an area and is trying to make the sound: [soundin]")
 		return
@@ -31,9 +69,9 @@
 		var/distance = get_dist(M, turf_source)
 
 		if(distance <= maxdistance)
-			M.playsound_local(turf_source, soundin, vol, vary, frequency, falloff, channel, pressure_affected, S)
+			M.playsound_local(turf_source, soundin, vol, vary, frequency, falloff, channel, pressure_affected, S, is_global)
 
-/mob/proc/playsound_local(turf/turf_source, soundin, vol as num, vary, frequency, falloff, channel = 0, pressure_affected = TRUE, sound/S)
+/mob/proc/playsound_local(turf/turf_source, soundin, vol as num, vary, frequency, falloff, channel = 0, pressure_affected = TRUE, sound/S, is_global)
 	if(!client || !can_hear())
 		return
 
@@ -43,6 +81,7 @@
 	S.wait = 0 //No queue
 	S.channel = channel || open_sound_channel()
 	S.volume = vol
+	S.environment = -1
 
 	if(vary)
 		if(frequency)
@@ -50,6 +89,7 @@
 		else
 			S.frequency = get_rand_frequency()
 
+	var/pressure_factor = 1.0
 	if(isturf(turf_source))
 		var/turf/T = get_turf(src)
 
@@ -60,7 +100,6 @@
 
 		if(pressure_affected)
 			//Atmosphere affects sound
-			var/pressure_factor = 1
 			var/datum/gas_mixture/hearer_env = T.return_air()
 			var/datum/gas_mixture/source_env = turf_source.return_air()
 
@@ -90,6 +129,32 @@
 
 		if(S.file == 'sound/goonstation/voice/howl.ogg' && distance > 0 && S.volume > 60 && isvulpkanin(src))
 			addtimer(CALLBACK(src, /mob/.proc/emote, "howl"), rand(10,30)) // Vulps cant resist! >)
+
+	if(!is_global)
+		if(istype(src,/mob/living/))
+			var/mob/living/carbon/M = src
+//			if (istype(M) && M.hallucination_power > 50 && M.chem_effects[CE_MIND] < 1)
+//				S.environment = PSYCHOTIC
+//			else if (M.druggy)
+			if (M.druggy)
+				S.environment = DRUGGED
+			else if (M.drowsyness)
+				S.environment = DIZZY
+			else if (M.confused)
+				S.environment = DIZZY
+			else if (M.stat == UNCONSCIOUS)
+				S.environment = UNDERWATER
+			else if (pressure_factor < 0.5)
+				S.environment = SPACE
+			else
+				var/area/A = get_area(src)
+				S.environment = A.sound_env
+
+		else if (pressure_factor < 0.5)
+			S.environment = SPACE
+		else
+			var/area/A = get_area(src)
+			S.environment = A.sound_env
 
 	SEND_SOUND(src, S)
 

egordmh/Paradise-old

code/game/sound.dm
@@ -1,4 +1,42 @@
-/proc/playsound(atom/source, soundin, vol as num, vary, extrarange as num, falloff, frequency = null, channel = 0, pressure_affected = TRUE, ignore_walls = TRUE)
+//Sound environment defines. Reverb preset for sounds played in an area, see sound datum reference for more.
+#define GENERIC 0
+#define PADDED_CELL 1
+#define ROOM 2
+#define BATHROOM 3
+#define LIVINGROOM 4
+#define STONEROOM 5
+#define AUDITORIUM 6
+#define CONCERT_HALL 7
+#define CAVE 8
+#define ARENA 9
+#define HANGAR 10
+#define CARPETED_HALLWAY 11
+#define HALLWAY 12
+#define STONE_CORRIDOR 13
+#define ALLEY 14
+#define FOREST 15
+#define CITY 16
+#define MOUNTAINS 17
+#define QUARRY 18
+#define PLAIN 19
+#define PARKING_LOT 20
+#define SEWER_PIPE 21
+#define UNDERWATER 22
+#define DRUGGED 23
+#define WOOZY 24
+#define PSYCHOTIC 25
+
+#define STANDARD_STATION STONEROOM
+#define LARGE_ENCLOSED HANGAR
+#define SMALL_ENCLOSED BATHROOM
+#define TUNNEL_ENCLOSED CAVE
+#define LARGE_SOFTFLOOR CARPETED_HALLWAY
+#define MEDIUM_SOFTFLOOR LIVINGROOM
+#define SMALL_SOFTFLOOR ROOM
+#define ASTEROID CAVE
+#define SPACE UNDERWATER
+
+/proc/playsound(atom/source, soundin, vol as num, vary, extrarange as num, falloff, frequency = null, channel = 0, pressure_affected = TRUE, ignore_walls = TRUE, var/is_global)
 	if(isarea(source))
 		error("[source] is an area and is trying to make the sound: [soundin]")
 		return
@@ -31,9 +69,9 @@
 		var/distance = get_dist(M, turf_source)
 
 		if(distance <= maxdistance)
-			M.playsound_local(turf_source, soundin, vol, vary, frequency, falloff, channel, pressure_affected, S)
+			M.playsound_local(turf_source, soundin, vol, vary, frequency, falloff, channel, pressure_affected, S, is_global)
 
-/mob/proc/playsound_local(turf/turf_source, soundin, vol as num, vary, frequency, falloff, channel = 0, pressure_affected = TRUE, sound/S)
+/mob/proc/playsound_local(turf/turf_source, soundin, vol as num, vary, frequency, falloff, channel = 0, pressure_affected = TRUE, sound/S, is_global)
 	if(!client || !can_hear())
 		return
 
@@ -43,6 +81,7 @@
 	S.wait = 0 //No queue
 	S.channel = channel || open_sound_channel()
 	S.volume = vol
+	S.environment = -1
 
 	if(vary)
 		if(frequency)
@@ -50,6 +89,7 @@
 		else
 			S.frequency = get_rand_frequency()
 
+	var/pressure_factor = 1.0
 	if(isturf(turf_source))
 		var/turf/T = get_turf(src)
 
@@ -60,7 +100,6 @@
 
 		if(pressure_affected)
 			//Atmosphere affects sound
-			var/pressure_factor = 1
 			var/datum/gas_mixture/hearer_env = T.return_air()
 			var/datum/gas_mixture/source_env = turf_source.return_air()
 
@@ -90,6 +129,32 @@
 
 		if(S.file == 'sound/goonstation/voice/howl.ogg' && distance > 0 && S.volume > 60 && isvulpkanin(src))
 			addtimer(CALLBACK(src, /mob/.proc/emote, "howl"), rand(10,30)) // Vulps cant resist! >)
+
+	if(!is_global)
+		if(istype(src,/mob/living/))
+			var/mob/living/carbon/M = src
+//			if (istype(M) && M.hallucination_power > 50 && M.chem_effects[CE_MIND] < 1)
+//				S.environment = PSYCHOTIC
+//			else if (M.druggy)
+			if (M.druggy)
+				S.environment = DRUGGED
+			else if (M.drowsyness)
+				S.environment = WOOZY
+			else if (M.confused)
+				S.environment = WOOZY
+			else if (M.stat == UNCONSCIOUS)
+				S.environment = UNDERWATER
+			else if (pressure_factor < 0.5)
+				S.environment = SPACE
+			else
+				var/area/A = get_area(src)
+				S.environment = A.sound_env
+
+		else if (pressure_factor < 0.5)
+			S.environment = SPACE
+		else
+			var/area/A = get_area(src)
+			S.environment = A.sound_env
 
 	SEND_SOUND(src, S)
 